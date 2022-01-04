c5204805907f0387ef5ce48c038711e48a262b16
PatrikVahlen <patrik.vahlen@gmail.com>
2022-01-04 14:37:46 +0100

    add

--- src/pages/LatestAddedPage.jsx
+++ src/pages/LatestAddedPage.jsx
@@ -1,14 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { UserInformation } from '../App';
 import { Link } from "react-router-dom"
 
 export default function LatestAddedPage() {
     const { companyList, setCompanyList } = useContext(UserInformation)
     const { userInformation, setUserInformation } = useContext(UserInformation)
+    const [arrayEmpty, setArrayEmtpy] = useState("")
+
+    useEffect(() => {
+        if (companyList.length == "0") {
+            setArrayEmtpy(null)
+        } else {
+            setArrayEmtpy(true)
+        }
+    });
+
     return (
         <div>
-            <p>Hi {userInformation.firstName} {userInformation.lastName}!</p>
-            <p>Your latest added company is {companyList[0].name}</p>
+            {console.log(arrayEmpty)}
+            {arrayEmpty ? (
+                <>
+                    <p>Hi {userInformation.firstName} {userInformation.lastName}!</p>
+                    <p>Your latest added company is {companyList[0].name}</p>
+                </>
+            ) : "Not found"}
             <Link to="/mypage">Home</Link>
         </div>
     )
